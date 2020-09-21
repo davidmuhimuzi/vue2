73d7ae6b33fdedbad85a71fb865b621187549841
Madi_Slocum <madisonslocum@sbcglobal.net>
2020-09-20 22:41:07 -0500

    excess files

--- src/main.js
+++ src/main.js
@@ -1,11 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
-
+import router from './router/index.js'
 Vue.config.productionTip = false
 
 new Vue({
+  router, //made changes here
   render: h => h(App),
 }).$mount('#app')
 
 // this file initializes your Vue application and signifies which HTML element 
 // in the index.html file your app should be attached to. 
+
+//Complete
