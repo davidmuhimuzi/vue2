15acaf8d79a9c0818880230b96078a9106261855
Madi_Slocum <madisonslocum@sbcglobal.net>
2020-09-24 21:03:15 -0500

    update

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,14 @@
   <div id="app">
     <div id="nav">
       <h1> Course Navigator </h1>
-    <img alt="OC logo" src="./assets/OC.png" height="200px" width="200px" >
+    <img alt="OC logo" src="./assets/OC.png" height="200px" width="199px" >
     <br>
+     
       <router-link to="/courseapi/courses"> List </router-link>|
       <router-link to="/courseapi/courses/add">  Add  </router-link>| 
+      <router-link to="/courseapi/courses/edit">  Edit  </router-link>| 
       <router-link to="/courseapi/courses/delete"> Delete </router-link>|
+      
     </div>
        <router-view />
   
