791204943852cfa2616a30719c68a752cae982b7
Madi_Slocum <madisonslocum@sbcglobal.net>
2020-09-27 15:45:55 -0500

    Update on courses

--- src/components/AddCourse.vue
+++ src/components/AddCourse.vue
@@ -1,39 +1,57 @@
 <template>
-  <div>
-    <H1>Add Course</H1>
-  
+  <div class="submit-form mt-3 mx-auto">
+    <H1>Add an OC Course</H1>
+
     <form @submit.prevent="saveCourse">
-      Id:
-      <input v-model="course.id" type="text" id="id">
+      <v-text-field 
+      v-model="course.id" 
+      label="ID" 
+      required></v-text-field>
 
-      <br>
-      <br>Department:
-      <input v-model="course.dept" type="text" id="dept">
+      <v-text-field
+        v-model="course.dept"
+        label="Department"
+        required
+      ></v-text-field>
 
-      <br>
-      <br>Course Number:
-      <input v-model="course.course_number" type="text" id="course_number">
-      
-      <br>
-      <br>Level:
-      <input v-model="course.level" type="text" id="level">
 
-      <br>
-      <br>Hours:
-      <input v-model="course.hours" type="text" id="hours">
-    
-      <br>
-      <br>Name:
-      <input v-model="course.name" type="text" id="name">
-      
-      <br>
-      <br>Description:
-      <input v-model="course.description" type="text" id="description">
+      <v-text-field
+        v-model="course.course_number"
+        label="Course #"
+        required
+      ></v-text-field>
 
-      <br>
-      <input type="submit" name="submit" value="Save">
-      <button name="cancel" v-on:click.prevent="cancel()">Cancel</button>
-    </form>
+      <v-text-field
+        v-model="course.level"
+        label="Level"
+        required
+      ></v-text-field>
+
+
+      <v-text-field
+        v-model="course.hours"
+        label="Hours"
+        required
+      ></v-text-field>
+
+
+      <v-text-field 
+      v-model="course.name" 
+      label="Name" 
+      required></v-text-field>
+
+      <v-text-field
+        v-model="course.description"
+        label="Description"
+        required
+      ></v-text-field>
+
+       </form>
+  <v-divider class="my-5"></v-divider>
+      <v-btn color="primary" class="mt-3" @click="saveCourse">Submit</v-btn>
+
+      <v-btn color="primary" class="mt-3" @click="cancel">Cancel</v-btn>
+
   </div>
 </template>
 
@@ -43,30 +61,38 @@ import CourseDataService from "../services/CourseDataService";
 export default {
   data() {
     return {
-      course: {},
-      submitted: false
+      course: {}
+      //submitted: false,
     };
   },
   methods: {
     saveCourse() {
       CourseDataService.create(this.course)
         .then(() => {
-          this.$router.push({ name: 'list' });
+          this.$router.push({ name: "list" });
         })
-        .catch(e => {
+        .catch((e) => {
           console.log(e);
         });
     },
-    cancel()  {
-        this.$router.push({ name: 'list' });
-    }
-  }
+    cancel() {
+      this.$router.push({ name: "list" });
+    },
+  },
 };
 </script>
 
 <style>
 .submit-form {
-  max-width: 300px;
+  max-width: 400px;
   margin: auto;
+  font-size: 20px;
 }
+
+
+h1 {
+  text-align: center;
+}
+
+
 </style>
